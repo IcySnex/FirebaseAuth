3f2c85024c4b38b3d348fdb6ffefe98fb52d4fba
IcySnex <lao43919@gmail.com>
2022-11-10 19:42:35 +0100

    Imported Endpoints from Firebase Authentication Documentation

---
+++ FirebaseAuth/Internal/Endpoints.cs
@@ -0,0 +1,24 @@
+﻿namespace FirebaseAuth.Internal;
+
+internal static class Endpoints
+{
+    public static string VerifyCustomToken = "https://identitytoolkit.googleapis.com/v1/accounts:signInWithCustomToken";
+
+    public static string SecureTokenGoogleApisCom = "https://securetoken.googleapis.com/v1/token";
+
+    public static string SignupNewUser = "https://identitytoolkit.googleapis.com/v1/accounts:signUp";
+
+    public static string VerifyPassword = "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword";
+
+    public static string VerifyAssertion = "https://identitytoolkit.googleapis.com/v1/accounts:signInWithIdp";
+
+    public static string CreateAuthUri = "https://identitytoolkit.googleapis.com/v1/accounts:createAuthUri";
+
+    public static string GetOobConfirmationCode = "https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode";
+
+    public static string ResetPassword = "https://identitytoolkit.googleapis.com/v1/accounts:resetPassword";
+
+    public static string SetAccountInfo = "https://identitytoolkit.googleapis.com/v1/accounts:update";
+
+    public static string GetAccountInfo = "https://identitytoolkit.googleapis.com/v1/accounts:lookup";
+}
