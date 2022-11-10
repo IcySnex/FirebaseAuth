8b083be85f7d33ce79bec8279d538360e2160e77
IcySnex <lao43919@gmail.com>
2022-11-10 19:37:25 +0100

    Added internal JSON converter

---
+++ FirebaseAuth/Exceptions/JsonObjectIsNullException.cs
@@ -0,0 +1,9 @@
+﻿namespace FirebaseAuth.Exceptions;
+
+public class JsonObjectIsNullException : Exception
+{
+    /// <summary>
+    /// Deserialized JSON Object is null
+    /// </summary>
+    public JsonObjectIsNullException() : base("Deserialized JSON Object is null") { }
+}
---
+++ FirebaseAuth/Internal/JsonHelper.cs
@@ -0,0 +1,40 @@
+﻿using FirebaseAuth.Exceptions;
+using System.Text.Json;
+
+namespace FirebaseAuth.Internal;
+
+internal class JsonHelper
+{
+    /// <summary>
+    /// Converts a json stirng into an object
+    /// </summary>
+    /// <typeparam name="T">The excpected return type</typeparam>
+    /// <param name="input">The json string to convert</param>
+    /// <param name="options">Options to control the conversion behavior</param>
+    /// <returns>A converted object representing the json string</returns>
+    /// <exception cref="ArgumentNullException">Occurs when json null is</exception>
+    /// <exception cref="JsonException">Occurs when the JSON is invalid. -or- TValue is not compatible with the JSON. -or- There is remaining data in the string beyond a single JSON value.</exception>
+    /// <exception cref="NotSupportedException">Occurs when there is no compatible System.Text.Json.Serialization.JsonConverter for TValue</exception>
+    /// <exception cref="JsonObjectIsNullException">Occurs when deserialized object does not represent the Type T (is null)</exception>
+    public static T Deserialize<T>(
+        string input,
+        JsonSerializerOptions? options = null)
+    {
+        return JsonSerializer.Deserialize<T>(input, options) is T result ?
+            result : throw new JsonObjectIsNullException();
+    }
+
+    /// <summary>
+    /// Converts an object into a json string
+    /// </summary>
+    /// <param name="input">The json string to convert</param>
+    /// <param name="options">Options to control the conversion behavior</param>
+    /// <returns>A json string representing the object</returns>
+    /// <exception cref="NotSupportedException">Occurs when there is no compatible System.Text.Json.Serialization.JsonConverter for TValue</exception>
+    public static string Serialize(
+        object input,
+        JsonSerializerOptions? options = null)
+    {
+        return JsonSerializer.Serialize(input, options);
+    }
+}
