c5a003c072b9bb1a9cca93f1f6f8d0be365290f8
IcySnex <lao43919@gmail.com>
2022-11-10 19:36:47 +0100

    Added configuration to setup AuthenticationProvider

---
+++ FirebaseAuth/Configuration/AuthenticationConfig.cs
@@ -0,0 +1,21 @@
+﻿namespace FirebaseAuth.Configuration;
+
+public class AuthenticationConfig
+{
+    /// <summary>
+    /// Creates a new new configuration for the Firebase Authentication API
+    /// </summary>
+    /// <param name="apiKey">The Firebase Web API key</param>
+    /// <param name="timeout">The time span in which a request times out</param>
+    public AuthenticationConfig(
+        string apiKey,
+        TimeSpan? timeout = null)
+    {
+        ApiKey = apiKey;
+        Timeout = timeout;
+    }
+
+
+    public string ApiKey { get; }
+    public TimeSpan? Timeout { get; }
+}
