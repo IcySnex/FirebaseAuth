75cbbc180ad6ac7c3e01fc85a4ee4a23aecd58fb
IcySnex <lao43919@gmail.com>
2022-11-12 18:27:15 +0100

    Added documenting on Endpoints

--- FirebaseAuth/Internal/Endpoints.cs
+++ FirebaseAuth/Internal/Endpoints.cs
@@ -5,23 +5,53 @@
 /// </summary>
 internal static class Endpoints
 {
+    /// <summary>
+    /// https://identitytoolkit.googleapis.com/v1/accounts:signInWithCustomToken
+    /// </summary>
     public static string VerifyCustomToken = "https://identitytoolkit.googleapis.com/v1/accounts:signInWithCustomToken";
 
+    /// <summary>
+    /// https://securetoken.googleapis.com/v1/token
+    /// </summary>
     public static string SecureTokenGoogleApisCom = "https://securetoken.googleapis.com/v1/token";
 
+    /// <summary>
+    /// https://identitytoolkit.googleapis.com/v1/accounts:signUp
+    /// </summary>
     public static string SignupNewUser = "https://identitytoolkit.googleapis.com/v1/accounts:signUp";
 
+    /// <summary>
+    /// https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword
+    /// </summary>
     public static string VerifyPassword = "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword";
 
+    /// <summary>
+    /// https://identitytoolkit.googleapis.com/v1/accounts:signInWithIdp
+    /// </summary>
     public static string VerifyAssertion = "https://identitytoolkit.googleapis.com/v1/accounts:signInWithIdp";
 
+    /// <summary>
+    /// https://identitytoolkit.googleapis.com/v1/accounts:createAuthUri
+    /// </summary>
     public static string CreateAuthUri = "https://identitytoolkit.googleapis.com/v1/accounts:createAuthUri";
 
+    /// <summary>
+    /// https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode
+    /// </summary>
     public static string GetOobConfirmationCode = "https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode";
 
+    /// <summary>
+    /// https://identitytoolkit.googleapis.com/v1/accounts:resetPassword
+    /// </summary>
     public static string ResetPassword = "https://identitytoolkit.googleapis.com/v1/accounts:resetPassword";
 
+    /// <summary>
+    /// https://identitytoolkit.googleapis.com/v1/accounts:update
+    /// </summary>
     public static string SetAccountInfo = "https://identitytoolkit.googleapis.com/v1/accounts:update";
 
+    /// <summary>
+    /// https://identitytoolkit.googleapis.com/v1/accounts:lookup
+    /// </summary>
     public static string GetAccountInfo = "https://identitytoolkit.googleapis.com/v1/accounts:lookup";
 }
